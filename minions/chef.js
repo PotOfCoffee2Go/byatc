803caf26fbe0a825ec67b8b0de87beb7ffeac448
PotOfCoffee2Go <kim@lrunit.net>
2017-04-30 00:42:29 +0000

    Added getting items on the frontend by category

--- minions/chef.js
+++ minions/chef.js
@@ -3,6 +3,7 @@
 (function () {
 
 const
+    alasql = require('alasql'),
     gearbox = require('./#gearing/gearbox'),
     
     MinionError = gearbox.MinionError,
@@ -14,6 +15,36 @@ var web = null;
 function Chef (Web) {
     web = Web;
 }
+
+Chef.prototype.onGetItemsByCategory = function onGetItemsByCategory(req, res, next, prayer) {
+    var
+        results = {},
+        catsheet = web.cfg.spreadsheets.sheets.find(r => r.alias === 'categories'), // guests,items,auction
+        itemsheet = web.cfg.spreadsheets.sheets.find(r => r.alias === 'items'); // guests,items,auction
+    try {
+        var catData = catsheet.db.getData('/');
+        var itemData = itemsheet.db.getData('/');
+        var catIds = Object.keys(catData);
+        catIds.forEach((catid) => {
+            //if (catid === 'C006') {
+                results[catid] = catData[catid];
+                results[catid].items = {};
+                var dbresult = alasql('SELECT * FROM ? AS item WHERE [1]->category->id = ?', [itemData, catid]);
+                dbresult.forEach((result) => {
+                    //delete result['1'].trello; delete result['1'].category;
+                    results[catid].items[result['0']] = result['1'];
+                });
+            //}
+        });
+    } catch(err) {
+        var error = new MinionError(minionName, 'Can not get data items by category', 101, err);
+        web.sendJson(null, res, web.minion.angel.errorPrayer(error, prayer));
+        return;
+    }
+    prayer.data = results;
+    web.sendJson(null, res, prayer);
+};
+
 
 Chef.prototype.onSelectFromSheetsDb = function onSelectFromSheetsDb(req, res, next, prayer) {
     var
@@ -69,15 +100,20 @@ Chef.prototype.onGetFromSheetsDb = function onGetFromSheetsDb(req, res, next, pr
         web.minion.chef.onSelectFromSheetsDb(req, res, next, prayer);
         return;
     }
+    if (prayer.resource.split('/')[3] === 'items' || prayer.resource.split('/')[4] === 'by' ||
+            prayer.resource.split('/')[5] === 'category') {
+        web.minion.chef.onGetItemsByCategory(req, res, next, prayer);
+        return;
+    }
     var recid = prayer.resource.split('/')[4],
         sheetAlias = prayer.resource.split('/')[3],
-        datastore = web.cfg.spreadsheets.sheets.find(s => s.alias === sheetAlias);
+        sheet = web.cfg.spreadsheets.sheets.find(s => s.alias === sheetAlias);
     try { // Remove the '/boss/clerk/alias' from resource to get the DB path
         var dataPath = prayer.resource.split('/').slice(4).join('/');
         if (recid)
-            prayer.data[recid] = datastore.db.getData('/' + dataPath);
+            prayer.data[recid] = sheet.db.getData('/' + dataPath);
         else
-            prayer.data = datastore.db.getData('/' + dataPath);
+            prayer.data = sheet.db.getData('/' + dataPath);
     } catch(err) {
         var error = new MinionError(minionName, 'Can not get data from Db', 101, err);
         web.sendJson(null, res, web.minion.angel.errorPrayer(error, prayer));
